b6087e3dd4c87c759d5e6c339dffa518c7e942a6
Brian Gao <zhdgao@gmail.com>
2025-09-20 15:51:17 -0700

    Adding Udemy Docker Course

--- dockerfiles/entrypoint/entrypoint-cmd-2/startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# do some startup stuff
-echo "Checking permissions on /var/www/html/upload"
-mkdir -p /var/www/html/upload
-chown -R www-data:www-data /var/www/html/upload
-
-# Start the application set in CMD
-exec "$@"
